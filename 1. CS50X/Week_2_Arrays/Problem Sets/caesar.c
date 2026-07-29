d0d96fbbfb38169dc2d30142d6b2480c42819212
Feivel Dieudonne Lucasan <lucasan.feiveldieudonne@gmail.com>
2026-07-29 15:58:32 +0800

    Update caesar.c

--- 1. CS50X/Week_2_Arrays/Problem Sets/caesar.c
+++ 1. CS50X/Week_2_Arrays/Problem Sets/caesar.c
@@ -3,23 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-char encipher (char c, int k);
+int only_digits(char *key);
+char encipher(char c, int k);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 2 || !only_digits(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
-    }
-
-    for (int i = 0; argv[1][i] != '\0'; i++)
-    {
-        if (!isdigit(argv[1][i]))
-        {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
     }
 
     char text[1000];
@@ -39,7 +31,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-char encipher (char c, int k)
+int only_digits(char *key)
+{
+    for (int i = 0; key[i] != '\0'; i++)
+    {
+        if (!isdigit(key[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+char encipher(char c, int k)
 {
     if (isupper(c))
     {
